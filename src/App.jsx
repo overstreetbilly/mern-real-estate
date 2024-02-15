5c5447fca180a9a264159e10c46cd43c378c5a0e
Billy Overstreet <overstreetbilly@gmail.com>
2024-02-15 12:19:28 -0700

    Install react and tailwind build first template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-9xl font-bold underline">Hello Tailwind world</h1>
+      <h1>Unstyled h1</h1>
+    </>
+  )
+}
+
+export default App
