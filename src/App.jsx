a45def3e2b6f377523c97bfe5daa560d68916f9d
Billy Overstreet <overstreetbilly@gmail.com>
2024-02-16 10:08:14 -0700

    updates to template

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 function App() {
   
 
   return (
-    <>
-      <h1 className="text-9xl font-bold underline">Hello Tailwind world</h1>
-      <h1>Unstyled h1</h1>
-    </>
+    
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/sign-in' element={<SignIn />}></Route>
+        <Route path='/sign-up' element={<SignUp />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+      </Routes>
+      </BrowserRouter>
+    
   )
 }
 
