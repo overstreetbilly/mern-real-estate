766513123a9e9a75f47f022a3ab245bc1883b0c8
Billy Overstreet <overstreetbilly@gmail.com>
2024-02-19 16:19:11 -0700

    Create sign up api route added error messaging

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL).then( ()=> {
     console.log('Connected to DB ok');
@@ -16,3 +19,4 @@ app.listen(3000, ()=>{
 } );
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
