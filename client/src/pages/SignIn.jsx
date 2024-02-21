1b0aefa6c9ab61f7ccc596ed81005af736bb9c1b
Billy Overstreet <overstreetbilly@gmail.com>
2024-02-21 16:32:33 -0700

    Added redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
     signInStart,
     signInSuccess,
