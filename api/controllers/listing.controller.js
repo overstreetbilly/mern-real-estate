b17058e93f4404394945bb2b09953dda22455dc9
Billy Overstreet <overstreetbilly@gmail.com>
2024-03-15 14:01:50 -0600

    Added update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -22,7 +22,6 @@ export const deleteListing = async (req, res, next ) =>{
         return next(errorHandler(401, 'You can only delete your own listings!'));
     }
 
-
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json('Listing has been deleted');
@@ -30,5 +29,27 @@ export const deleteListing = async (req, res, next ) =>{
     } catch (error) {
         next(error);
     }
-
 };
+
+export const updateListing = async (req, res, next)=>{
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if (req.user.id !== listing.userRef){
+        return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+
+}
