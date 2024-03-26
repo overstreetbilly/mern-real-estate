c6d409a68223a8c88524b350aba74cb9943b2836
Billy Overstreet <overstreetbilly@gmail.com>
2024-03-26 11:17:29 -0600

    Finished about page changed title element

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,29 @@
 const About = () => {
-    return <div>About</div>;
+    return (
+        <div className='py-20 px-4 max-w-6xl mx-auto'>
+            <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+                About This Site
+            </h1>
+            <p className='mb-4 text-slate-700'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                doloribus itaque minima necessitatibus nisi ullam, facilis
+                autem. Porro, assumenda? Expedita distinctio labore itaque
+                reiciendis aut minima magnam, soluta voluptatibus repellat?
+            </p>
+            <p className='mb-4 text-slate-700'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo,
+                magnam sapiente porro quidem dicta magni adipisci totam rem
+                earum culpa nemo odio beatae cumque ipsa. Quo placeat expedita
+                voluptatibus illum.
+            </p>
+            <p className='mb-4 text-slate-700'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste
+                rem blanditiis doloremque sapiente non recusandae similique
+                eaque atque. Esse culpa saepe tempora adipisci voluptatibus non
+                labore laborum sunt laudantium libero.
+            </p>
+        </div>
+    );
 };
 
 export default About;
